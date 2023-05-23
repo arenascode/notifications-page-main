b4c220053bd2f055d870817519458813d8606908
Miguel Arenas <migue.arenas91@gmail.com>
2023-05-22 21:17:44 -0500

    project finished

--- js/index.js
+++ js/index.js
@@ -3,7 +3,9 @@ const NotifCount = document.getElementById('notificationsCount')
 
 //notifications
 const notifications = document.getElementsByClassName('notification')
-const circleNotif1 = document.getElementById("circle1");const circleNotif2 = document.getElementById("circle2");const circleNotif3 = document.getElementById("circle3");
+const circleNotif1 = document.getElementById("circle1");
+const circleNotif2 = document.getElementById("circle2");
+const circleNotif3 = document.getElementById("circle3");
 
 function markAllAsRead() {
   for (let i = 0; i <= 2; i++) {
